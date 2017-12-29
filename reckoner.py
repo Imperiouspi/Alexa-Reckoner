001db1b44196d6f55f02ea2e53eae74934e71027
Imperiouspi <nitrogen@cassidys.ca>
2017-12-28 22:13:57 -0600

    Headline v1

--- reckoner.py
+++ reckoner.py
@@ -28,5 +28,13 @@ def points(house):
 
     return statement(render_template("points", h=house, p=450))
 
+@ask.intent("Headline")
+def headline():
+    featurebox = reckonerPage.find(class_='module featured-posts-slider-module et_pb_extra_module mobile-text et_pb_featured_posts_slider_0 et_pb_bg_layout_dark')[0].div.article.div.div
+    title = featurebox.h3.a.contents
+    author = featurebox.div.p.a.contents
+    date = featurebox.div.p.span.contents
+    return statement(render_template("headline", t=title, a=author, d='date'))
+
 if __name__ == "__main__":
     app.run(debug=True)
